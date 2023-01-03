41014ab3117d4a6e30cb32b45c2cbcc289e094f1
m19e <nexus.me7290@gmail.com>
2023-01-03 16:26:50 +0900

    Remove head for app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,13 @@
 import type { AppProps } from "next/app"
-import Head from "next/head"
 import { ThemeProvider } from "next-themes"
 import "@/styles/globals.css"
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <Head>
-        <title>【こころの色、恋のうた】櫻木 真乃</title>
-      </Head>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
-export default MyApp
+export default App
