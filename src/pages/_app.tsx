7ed23a75548d134f9d9bda2b7ec433acdcc19841
m19e <nexus.me7290@gmail.com>
2023-06-12 18:36:42 +0900

    feat: fix import css path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app"
 import { ThemeProvider } from "next-themes"
-import "@/styles/globals.css"
+import "styles/globals.css"
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
